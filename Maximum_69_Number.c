baca97df4b3cc50005ee18e0be250ec857ee868f
KallepalliDurgabhavani <119723747+KallepalliDurgabhavani@users.noreply.github.com>
2023-05-23 19:00:48 +0530

    #include<stdio.h> int main() {     char s[100];     scanf("%s",&s);     for(int i=0;s[i]!=NULL;i++)     {         if(s[i]=='6')         {             s[i]='9';             break;         }     }     printf("%s",s); }

---
+++ Maximum_69_Number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    char s[100];
+    scanf("%s",&s);
+    for(int i=0;s[i]!=NULL;i++)
+    {
+        if(s[i]=='6')
+        {
+            s[i]='9';
+            break;
+        }
+    }
+    printf("%s",s);
+}
