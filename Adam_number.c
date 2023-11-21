1f563119589954c4f7eadf14e3bf541313a158e1
KallepalliDurgabhavani <119723747+KallepalliDurgabhavani@users.noreply.github.com>
2023-11-21 15:45:29 +0530

    #include<stdio.h> int rev(int n) {     int r,s=0;     while(n!=0)     {         r=n%10;         s=s*10+r;         n=n/10;     }     return s; } int main() {     int n;     scanf("%d",&n);     int s=n*n;     int re=rev(n);     int sq=re*re;     if(s==rev(sq))     {         printf("True");     }     else     {         printf("False");     } }

--- Adam_number.c
+++ Adam_number.c
@@ -1,27 +1,28 @@
 #include<stdio.h>
 int rev(int n)
 {
-    int r=0,p;
+    int r,s=0;
     while(n!=0)
     {
-        p=n%10;
-        r=r*10+p;
+        r=n%10;
+        s=s*10+r;
         n=n/10;
     }
-    return r;
+    return s;
 }
 int main()
 {
-    int n,s,rs,sq;
+    int n;
     scanf("%d",&n);
-    s=n*n;
-    rs=rev(n);
-    sq=rs*rs;
-    if(rev(sq)==s)
+    int s=n*n;
+    int re=rev(n);
+    int sq=re*re;
+    if(s==rev(sq))
     {
         printf("True");
     }
-    else{
+    else
+    {
         printf("False");
     }
 }
